a27220f77ab2dbd199314ca15cd94786aefdec92
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-06-27 16:36:29 +0530

    file update

--- Food_UI/src/App.jsx
+++ Food_UI/src/App.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-
 const Home = () => <div className="p-4">Home Page Content</div>;
-const About = () => <div className="p-4">About Page Content</div>;
-const Services = () => <div className="p-4">Services Page Content</div>;
+const Ouroutlets = () => <div className="p-4">Our Outlets Content</div>;
+const Order = () => <div className="p-4">Order Page Content</div>;
+const Contact = () => <div className="p-4">Contact Page Content</div>;
+
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Navbar />
       <Routes>
-        <Route>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="services" element={<Services />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="ouroutlets" element={<Ouroutlets />} />
+          <Route path="order" element={<Order />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- Food_UI/src/components/Navbar.jsx
+++ Food_UI/src/components/Navbar.jsx
@@ -1,29 +1,32 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo_Navbar from "../assets/logo_navbar.png";
 
 const Navbar = () => {
   return (
-    <>
     <div className="container">
-      <nav className="fixed w-full z-20 top-0 start-0 border-1   ">
+      <nav className="fixed w-full z-20 top-0 start-0 border-1">
         <div className="max-w-full flex flex-wrap items-center justify-between ml-[50px] p-4">
           <NavLink
             to="/"
             className="flex items-center space-x-4 rtl:space-x-reverse"
           >
             <img src={Logo_Navbar} className="h-9" alt="Burger Logo" />
-            <span className="self-center text-[24px] text-heading text-white font-semibold whitespace-nowrap bg">
+            <span className="self-center text-[24px] text-heading text-white font-semibold whitespace-nowrap">
               BURGER
             </span>
           </NavLink>
-          <div className="hidden w-full md:block md:w-auto mx-auto" id="navbar-default">
+
+          <div
+            className="hidden w-full md:block md:w-auto mx-auto"
+            id="navbar-default"
+          >
             <ul className="font-medium flex flex-col p-4 gap-x-20 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
               <li>
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
-                    `  block py-2 px-3 rounded md:p-0 ${isActive ? "text-fg-brand" : "text-heading"}`
+                    `block py-2 px-3 rounded md:p-0 ${isActive ? "text-fg-brand" : "text-heading"}`
                   }
                 >
                   Home
@@ -31,22 +34,32 @@ const Navbar = () => {
               </li>
               <li>
                 <NavLink
-                  to="/about"
+                  to="/ouroutlets"
                   className={({ isActive }) =>
                     `block py-2 px-3 rounded md:p-0 ${isActive ? "text-fg-brand" : "text-heading"}`
                   }
                 >
-                  About
+                  Our outlets
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  to="/services"
+                  to="/order"
                   className={({ isActive }) =>
                     `block py-2 px-3 rounded md:p-0 ${isActive ? "text-fg-brand" : "text-heading"}`
                   }
                 >
-                  Services
+                  Order
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `block py-2 px-3 rounded md:p-0 ${isActive ? "text-fg-brand" : "text-heading"}`
+                  }
+                >
+                  Contact
                 </NavLink>
               </li>
             </ul>
@@ -54,10 +67,6 @@ const Navbar = () => {
         </div>
       </nav>
     </div>
-      <main className="pt-20">
-        <Outlet />
-      </main>
-    </>
   );
 };
 
